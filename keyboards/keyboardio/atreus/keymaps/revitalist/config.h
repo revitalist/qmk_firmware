11c35f85ac674e192185496808cd63ffb6dd089b
revitalist <lfboshoff@gmail.com>
2024-08-02 21:53:17 -0300

    Added a license to the revitalist config.h file

--- keyboards/keyboardio/atreus/keymaps/revitalist/config.h
+++ keyboards/keyboardio/atreus/keymaps/revitalist/config.h
@@ -1,3 +1,19 @@
+/* Copyright (C) 2019, 2020  Keyboard.io, Inc
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #define U_NP KC_NO // key is not present
 #define U_NA KC_NO // present but not available for use
 #define U_NU KC_NO // available but not used
